24a707a5670af18fd88c312608349f50b01703d4
vukasinvujic <vukasinvujic021@gmail.com>
2021-10-02 11:24:55 +0200

    send data from one to another component with servise

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -13,7 +13,11 @@ export class NewAccountComponent {
   constructor(
     private logginService: LoggingService,
     private accountsService: AccountsService
-  ) {}
+  ) {
+    this.accountsService.statusUpdate.subscribe((status: string) =>
+      alert('New status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
